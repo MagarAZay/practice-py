e539239c9ba49123df022af80d99606f63d6229b
Yazasiohw <magarazay6@gmail.com>
2026-06-24 20:45:41 +0545

    Implement string to number conversion function

---
+++ string_to_number.py
@@ -0,0 +1,7 @@
+def string_to_number(s):
+    return int(s)
+print(string_to_number("89"))
+print(string_to_number("33"))
+print(string_to_number("422"))
+print(string_to_number("44424424424244444424444444444445555"))
+print(string_to_number("98347679837649374698777539876964795558u9678759"))
