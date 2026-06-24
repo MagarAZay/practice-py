c82563eb5c4b8ccfc1bdb955d9e605091e94ebc5
Yazasiohw <magarazay6@gmail.com>
2026-06-24 20:38:14 +0545

    Implement number to string conversion function

---
+++ numbertostr.py
@@ -0,0 +1,7 @@
+def number_to_string(num):
+    return (f"{num}")
+print(number_to_string(23))
+print(number_to_string(32))
+print(number_to_string(4432))
+print(number_to_string(1213))
+print(number_to_string(24344442344432353424655744745773))
