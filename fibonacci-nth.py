b5608677871219f5646b5075c3a30fdfc2ad62fc
Yazasiohw <magarazay6@gmail.com>
2026-06-26 12:45:27 +0545

    Refactor fibonacci function to nth_fib function

--- fibonacci-nth.py
+++ fibonacci-nth.py
@@ -1,6 +1,11 @@
-def fibonacci(n):
-  a, b = 0, 1
-  for i in range(n-1):
-    a, b = b, a+b
-  return a
+def nth_fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
 print(fibonacci(10))
